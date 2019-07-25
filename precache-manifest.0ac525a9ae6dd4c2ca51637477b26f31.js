430f58e494e6a3d9472286461b7fb18ee0684099
faudzif <passion_farhan@yahoo.com>
2019-07-25 14:54:16 +0500

    first commit

---
+++ precache-manifest.0ac525a9ae6dd4c2ca51637477b26f31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6aceea2c3a25d46f6521b780956fe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "130fe6d29999baf64562",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f279bb0c8886f9741752",
+    "url": "/static/js/2.bf3f4520.chunk.js"
+  },
+  {
+    "revision": "130fe6d29999baf64562",
+    "url": "/static/js/main.5a272b77.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
